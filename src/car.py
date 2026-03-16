c59e208eff51d5824d33618058899d150a6c9a96
kwfn <lisitsyn.toha@list.ru>
2026-03-17 02:37:05 +0300

    Update car.py

--- src/car.py
+++ src/car.py
@@ -13,9 +13,7 @@ class Car:
         self._fuel_in_tank += fuel_quantity
 
     def drive(self, distance_km: float):
-        # Считаем, что расход 8 литров на 100 км
         fuel_burned: int = 8 * (distance_km / 100)
-        # TODO: Вася, не забудь расскомментировать! Клиенты могут застрять!!11
         if self._fuel_in_tank < fuel_burned:
             raise Exception("Не доедем жеж...")
         self._fuel_in_tank -= fuel_burned
